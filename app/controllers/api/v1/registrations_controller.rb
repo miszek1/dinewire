817733e42c975d0a62bfeabd6bccff4c1c197458
Scott Steinbeck <uniquetrio2000@gmail.com>
2022-07-25 20:52:33 -0700

    add account deletion

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_by_token, only: [:cancel]
   respond_to :json
-	
+	def cancel
+		@current_user.messages.destroy_all
+		@current_user.destroy
+		render json: {message:"Account Removed"}, status: 200
+	end
 	def sign_up_params
 		params.require(:user).permit(:email, :password, :password_confirmation,:first_name, :last_name, :user_name)
 	end
-
 end
